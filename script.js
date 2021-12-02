1f15a7ab080e5eabf7a0fdb9bbf7d469fdb6bb3e
Mary <mary8135@gmail.com>
2021-12-02 21:09:49 +0000

    start script file. QUIT statement added

---
+++ script.js
@@ -0,0 +1,11 @@
+let input = prompt('What would you like to do?');
+
+let myTodoList = ['learn to code', 'stop being poor']
+
+while (input !== 'quit'){
+    
+    input = prompt ('what would you like to do?');
+
+}
+
+console.log('OK. Quit App.');
