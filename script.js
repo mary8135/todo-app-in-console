a0fc4d4509b18582b744ffe81d22f4b30a646f9b
Mary <mary8135@gmail.com>
2021-12-02 21:21:37 +0000

    add loop for LIST

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let input = prompt('What would you like to do?');
 
-let myTodoList = ['learn to code', 'stop being poor']
+const myTodoList = ['learn to code', 'stop being poor'];
 
 while (input !== 'quit'){
-    
+    if (input === 'list'){
+        console.log('**********');
+        for(let i = 0; i < myTodoList.length; i++){
+            console.log`${i}: ${myTodoList[i]}`;
+        }
+        console.log('**********');
+    }
     input = prompt ('what would you like to do?');
 
 }
